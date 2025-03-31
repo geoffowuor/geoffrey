4c1f31ae59f8be74848d4c3f108be5077225f312
geoffrey <geoffreygroovy@gmail.com>
2025-03-31 20:11:06 +0300

    nav component

---
+++ Revamp/portfolio/src/Nav.jsx
@@ -0,0 +1,15 @@
+function Nav(){
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+
+}
+
+export default Nav
