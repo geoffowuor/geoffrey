85b503ae8ff6765d96f3fa317a005958de8b6435
geoffrey <geoffreygroovy@gmail.com>
2025-03-31 21:07:59 +0300

    fixed an issue

---
+++ Revamp/portfolio/src/Nav.jsx
@@ -0,0 +1,22 @@
+function Nav(){
+    return (
+        <div>
+        <nav>
+        
+            <a href="#"><h2 className="logo">Geoffrey</h2></a> 
+        <ul>
+          <li><a href="#about">Me</a></li>
+          <li><a href="#skills">Skills</a></li>
+            <li><a href="#projects">Projects</a></li>
+          <li> <button> <a href="#">Resume</a></button></li>
+
+          
+            
+        </ul>
+    
+        </nav>
+        </div>
+    )
+}
+
+export default Nav
